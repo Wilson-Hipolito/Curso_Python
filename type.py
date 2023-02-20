5cb459bed78413a80169fec51e42aa40db4d8efb
Wilson Hipólito <wilsonhipolito446@gmail.com>
2023-02-20 17:51:57 -0300

    Strings e Type

---
+++ type.py
@@ -0,0 +1,11 @@
+a = "Olá Mundo"
+b = 3
+c = 2.5
+d = 1+3j
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+# Quando precisar saber qual o tipo de dado armazenado na variável, podemos usar o Type 
