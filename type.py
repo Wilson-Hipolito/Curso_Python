f8024858fd419e644fdbc26f47cf7c61cb174bd3
Wilson Hipólito <wilsonhipolito446@gmail.com>
2023-03-05 17:21:08 -0300

    Atualização

--- type.py
+++ type.py
@@ -3,9 +3,8 @@ b = 3
 c = 2.5
 d = 1+3j
 
+#Quando precisar saber qual o tipo de dado armazenado na variável, podemos usar o Type.
 print(type(a))
 print(type(b))
 print(type(c))
 print(type(d))
-
-# Quando precisar saber qual o tipo de dado armazenado na variável, podemos usar o Type 
