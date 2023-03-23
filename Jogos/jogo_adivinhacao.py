fac0610a8b0b40441abb77269a7f29c822fefa2f
Wilson Hipólito <wilsonhipolito446@gmail.com>
2023-03-23 20:38:49 -0300

    Estudando Python.

--- Jogos/jogo_adivinhacao.py
+++ Jogos/jogo_adivinhacao.py
@@ -3,11 +3,28 @@ print("Bem Vindo ao Jogo de Adivinhação!!!")
 print("***********************************\n")
 
 numero_secreto = 42
+tentativas = 3
+rodadas = 1
 
-chute = input("Digite o seu número: ")
-print("Você digitou:", chute)
+while rodadas <= tentativas:
+    print("Tentativa:", rodadas, "de", tentativas)
+    chute = input("Digite o seu número: ")
+    print("Você digitou:", chute)
+    chute = int(chute)
 
-if numero_secreto == int(chute):
-    print("Você acertou!")
-else:
-    print("Você errou!")
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print("Parabéns, você acertou!\n")
+    else:
+        if(maior):
+            print("Você errou! Seu chute foi maior que o número secreto.\n")
+        elif(menor):
+            print("Você errou! Seu chute foi menor que o número secreto.\n")
+
+    rodadas = rodadas + 1
+    print("***********************************")
+    print("Fim do jogo!!!")
+    print("***********************************\n")
