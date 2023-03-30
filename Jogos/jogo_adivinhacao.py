04afe4a8943e8a24aa709d1ea0565dba00c262c1
Wilson Hipólito <wilsonhipolito446@gmail.com>
2023-03-30 15:55:34 -0300

    Estudando Python

--- Jogos/jogo_adivinhacao.py
+++ Jogos/jogo_adivinhacao.py
@@ -6,11 +6,15 @@ numero_secreto = 42
 tentativas = 3
 rodadas = 1
 
-while rodadas <= tentativas:
-    print("Tentativa:", rodadas, "de", tentativas)
-    chute = input("Digite o seu número: ")
+for rodadas in range(1, tentativas + 1):
+    print("Tentativa {} de {}.".format(rodadas, tentativas))
+    chute = input("Digite um número entre 1 e 50: ")
     print("Você digitou:", chute)
     chute = int(chute)
+
+    if(chute < 1 or chute > 50):
+        print("\nVocê digitou um número inválido!!!.\n")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -18,13 +22,13 @@ while rodadas <= tentativas:
 
     if(acertou):
         print("Parabéns, você acertou!\n")
+        break
     else:
         if(maior):
             print("Você errou! Seu chute foi maior que o número secreto.\n")
         elif(menor):
             print("Você errou! Seu chute foi menor que o número secreto.\n")
 
-    rodadas = rodadas + 1
     print("***********************************")
     print("Fim do jogo!!!")
     print("***********************************\n")
