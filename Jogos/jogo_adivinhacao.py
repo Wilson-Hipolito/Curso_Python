3261ccc6037dadbab65c23519b5d4b172a198d1e
Wilson Hipólito <wilsonhipolito446@gmail.com>
2023-04-07 14:55:06 -0300

    Estudando Python.

--- Jogos/jogo_adivinhacao.py
+++ Jogos/jogo_adivinhacao.py
@@ -1,52 +1,57 @@
 import random
 
-print("***********************************")
-print("Bem Vindo ao Jogo de Adivinhação!!!")
-print("***********************************\n")
+def jogo():
 
-numero_secreto = random.randrange(1, 51)
-tentativas = 0
-rodadas = 1
-pontos = 1000
+    print("***********************************")
+    print("Bem Vindo ao Jogo de Adivinhação!!!")
+    print("***********************************\n")
 
-print("Escolha o nível de dificuldade, sendo: (1)Fácil (2)Médio (3)Difícil.")
-nivel = int(input("Defina o nível: "))
+    numero_secreto = random.randrange(1, 51)
+    tentativas = 0
+    rodadas = 1
+    pontos = 1000
 
-if(nivel == 1):
-    tentativas = 20
-elif(nivel == 2):
-    tentativas = 10
-elif(nivel == 3):
-    tentativas = 5
-else:
-    print("Você digitou um nível inválido, tente novamente.\n")
+    print("Escolha o nível de dificuldade, sendo: (1)Fácil (2)Médio (3)Difícil.")
+    nivel = int(input("Defina o nível: "))
 
-for rodadas in range(1, tentativas + 1):
-    print("Tentativa {} de {}.".format(rodadas, tentativas))
-    
-    chute = input("Digite um número entre 1 e 50: ")
-    print("Você digitou:", chute)
-    chute = int(chute)
-
-    if(chute < 1 or chute > 50):
-        print("Você digitou um número inválido!!!.")
-        continue
-
-    acertou = chute == numero_secreto
-    maior = chute > numero_secreto
-    menor = chute < numero_secreto
-
-    if(acertou):
-        print("Parabéns, você acertou e fez {} pontos!".format(pontos))
-        break
+    if(nivel == 1):
+        tentativas = 20
+    elif(nivel == 2):
+        tentativas = 10
+    elif(nivel == 3):
+        tentativas = 5
     else:
-        if(maior):
-            print("Você errou! Seu chute foi maior que o número secreto.")
-        elif(menor):
-            print("Você errou! Seu chute foi menor que o número secreto.")
-        pontos_perdidos = abs(numero_secreto - chute)
-        pontos = pontos - pontos_perdidos
+        print("Você digitou um nível inválido, tente novamente.\n")
 
-print("***********************************")
-print("Fim do jogo!!!")
-print("***********************************\n")
+    for rodadas in range(1, tentativas + 1):
+        print("Tentativa {} de {}.".format(rodadas, tentativas))
+        
+        chute = input("Digite um número entre 1 e 50: ")
+        print("Você digitou:", chute)
+        chute = int(chute)
+
+        if(chute < 1 or chute > 50):
+            print("Você digitou um número inválido!!!.")
+            continue
+
+        acertou = chute == numero_secreto
+        maior = chute > numero_secreto
+        menor = chute < numero_secreto
+
+        if(acertou):
+            print("Parabéns, você acertou e fez {} pontos!".format(pontos))
+            break
+        else:
+            if(maior):
+                print("Você errou! Seu chute foi maior que o número secreto.")
+            elif(menor):
+                print("Você errou! Seu chute foi menor que o número secreto.")
+            pontos_perdidos = abs(numero_secreto - chute)
+            pontos = pontos - pontos_perdidos
+
+    print("***********************************")
+    print("Fim do jogo!!!")
+    print("***********************************\n")
+
+if (__name__ == "__main__"):
+    jogo()
---
+++ Jogos/jogos.py
@@ -0,0 +1,24 @@
+import forca
+import jogo_adivinhacao
+
+def escolhe_jogo():
+
+    print("***********************************")
+    print("******Escolha o Seu Jogo!!!********")
+    print("***********************************\n")
+
+    print("Jogo da Forca (1) | Jogo De Adivinhação (2)")
+
+    jogo = int(input("Qual Jogo Você Escolhe? "))
+
+    if jogo == 1:
+        print("Voce Escolheu o Jogo da Forca.")
+        forca.jogo()
+    elif jogo == 2:
+        print("Você escolheu o Jogo de Adivinhação.")
+        jogo_adivinhacao.jogo()
+    else:
+        print("Você Não Escolheu Uma Opção Valida.")
+
+if (__name__ == "__main__"):
+    escolhe_jogo()
