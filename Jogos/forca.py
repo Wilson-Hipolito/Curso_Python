fba8b492ab67f4e137598e39dc5ee73728955367
Wilson Hipólito <wilsonhipolito446@gmail.com>
2023-04-10 19:21:08 -0300

    Estudando Python - Módulo 2.

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -1,9 +1,23 @@
 def jogo():
-
     print("***********************************")
     print("***Bem Vindo ao Jogo de Forca!!!***")
     print("***********************************\n")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False 
+
+    while(not enforcou and not acertou):
+        
+        chute = input("Qual letra?")
+        posicao = 0
+
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {}, na posição {}.".format(letra, posicao))
+            posicao = posicao + 1
+
+        print("Jogando......")
 
     print("***********************************")
     print("**********Fim do jogo!!!***********")
