bf0eaef3341a9a41c70e2c1b5474145607e99bbb
Wilson Hipólito <wilsonhipolito446@gmail.com>
2023-04-21 17:07:11 -0300

    Estudando Python - Módulo 2.

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -1,33 +1,57 @@
+import random
+
 def jogo():
-    print("\n\n***********************************")
-    print("***Bem Vindo ao Jogo de Forca!!!***")
+    print("\n***********************************")
+    print("   BEM VINDO AO JOGO DA FORCA !!!  ")
     print("***********************************\n")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ['_', '_', '_', '_', '_', '_']
-    enforcou = False 
-    acertou = False 
+    arquivo = open("palavras.txt", "r")
+    palavras = []
 
-    print(letras_acertadas, "\n")
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
 
-    while(not enforcou and not acertou):
-        
-        chute = input("Qual letra? ")
-        chute = chute.strip()
-        
-        posicao = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[posicao] = letra 
-            posicao = posicao + 1
+    arquivo.close()
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
 
-        print(letras_acertadas)
+    letras_acertadas = ['_' for letra in palavra_secreta]
+    enforcou = False
+    acertou = False
+    erros = 6
+
+    print("Palavra Secreta:", letras_acertadas)
+
+    while (not enforcou and not acertou):
+
+        chute = input("\nTente acertar, digite uma letra: ")
+        chute = chute.strip().upper()
+
+        if (chute in palavra_secreta):
+            print("Parabéns você acertou a letra !!!")
+            posicao = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[posicao] = letra
+                posicao += 1
+        else:
+            print("Você errou a letra !!!")
+            erros += -1
+        enforcou = erros == 0
+        acertou = "_" not in letras_acertadas
+        print("Palavra Secreta: ", letras_acertadas)
         letras_faltando = str(letras_acertadas.count('_'))
-        print("Ainda faltam acertar {} letras.".format(letras_faltando))
-
-    print("***********************************")
-    print("**********Fim do jogo!!!***********")
+        print("Faltam acertar {} letras.".format(letras_faltando))
+        print("Você possui {} tentativas.".format(erros))
+    if (acertou):
+        print("\n VOCÊ GANHOU, PARABÉNS !!!")
+    else:
+        print("\nVOCÊ PERDEU, TENTE NOVAMENTE !!!")
+    print("\n***********************************")
+    print("**********FIM DO JOGO !!!**********")
     print("***********************************\n")
+
 
 if (__name__ == "__main__"):
     jogo()
