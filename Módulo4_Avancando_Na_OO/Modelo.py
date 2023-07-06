59b142b3352cf6281fdb3f7a566927cbb96c65fe
Wilson Hipólito <wilsonhipolito446@gmail.com>
2023-07-06 19:50:41 -0300

    Estudando Python_3.
    Começando módulo novo e avançando no aprendizado de Python3.

---
+++ Módulo4_Avancando_Na_OO/Modelo.py
@@ -0,0 +1,19 @@
+class Filme:
+    def __init__(self, nome, ano, duracao):
+        self.nome = nome
+        self.ano = ano
+        self.duracao = duracao
+
+
+vingadores = Filme('Vingadores - Guerra Infinita', 2018, 160)
+print(f"Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}")
+
+class Serie:
+    def __init__(self, nome, ano, temporadas):
+        self.nome = nome
+        self.ano = ano
+        self.temporadas = temporadas
+
+
+atlanta = Serie("Game Of Thrones", 2018, 2)
+print(f"Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas}")
