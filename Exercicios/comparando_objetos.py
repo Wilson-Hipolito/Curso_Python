6ccac5373741ece71fcf7f8e1c69593daa79e0c0
Wilson Hipólito <wilsonhipolito446@gmail.com>
2023-07-24 19:54:49 -0300

    Estudando Python_3.

--- Exercicios/comparando_objetos.py
+++ Exercicios/comparando_objetos.py
@@ -8,6 +8,9 @@ class Filmes():
     def __str__(self):
         return self.titulo + ' - ' + self.diretor
 
+    def __eq__(self, outro_filme):
+        return self.titulo == outro_filme.titulo
+
 
 teoria_de_tudo = Filmes('A Teoria de Tudo', 'James Marsh')
 la_la_land = Filmes('La La Land', 'Damien Chazelle')
@@ -22,3 +25,17 @@ def pega_todos_os_filmes():
 meus_filmes = pega_todos_os_filmes()
 for filme in meus_filmes:
     print(filme)
+
+
+def tenho_o_filme(filme_procurado):
+    meus_filmes = pega_todos_os_filmes()
+    return filme_procurado in meus_filmes
+
+
+filme_procurado = Filmes('A Teoria de Tudo', 'James Marsh')
+
+
+if tenho_o_filme(filme_procurado):
+    print('\nTenho o Filme!')
+else:
+    print('\nNão Tenho :(')
