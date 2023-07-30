05e63ffc487b9bbae6487d90f2ec25231868945b
Wilson Hipólito <wilsonhipolito446@gmail.com>
2023-07-30 16:08:00 -0300

    create jogo_quiz.

---
+++ Jogos/jogo_quiz.py
@@ -0,0 +1,18 @@
+
+
+quiz = {
+    'Qual banda é conhecida como "A Maior Banda de Rock do Mundo"?': {
+        'resposta':'The Rolling Stones',
+        'opcoes': ['Guns N Roses', 'Metallica', 'Led Zeppelin'],
+        "pontuacao": 10
+    },
+    'Qual álbum é considerado um dos maiores sucessos de todos os tempos e apresenta a música "Bohemian Rhapsody"?': {
+        'resposta':'A Night at the Opera (Queen)',
+        'opcoes': ['Appetite For Destruction', 'Black Albun', 'Get a Grip'],
+        'pontuacao': 5
+    },
+    # Outras perguntas e respostas...
+}
+
+print('\n Bem Vindo ao Quiz - "Curiosidades do Rock".')
+print('---------------------------------------------\n')
