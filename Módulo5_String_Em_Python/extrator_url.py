62309e7862bd88591a3f866deb52b6ece818ea8a
Wilson Hipólito <wilsonhipolito446@gmail.com>
2023-08-14 19:45:49 -0300

    Estudando Python_3.

--- Módulo5_String_Em_Python/extrator_url.py
+++ Módulo5_String_Em_Python/extrator_url.py
@@ -2,6 +2,7 @@ class ExtratorUrl:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
         self.valida_url()
+        self.valida_url_base()
 
     def sanitiza_url(self, url):
         if type(url) == str:
@@ -18,13 +19,11 @@ class ExtratorUrl:
         url_base = self.url[:indice_interrogacao]
         return url_base
 
-    def valida_inicio_url(self):
-        verifica_https = self.url.startswith('https')
-        if not verifica_https:
-        
-
-    def verifica_fim(self):
-        pass
+    def valida_url_base(self):
+        if not self.get_url_base().startswith('https'):
+            raise ValueError('Https não encontrado.')
+        if not self.get_url_base().endswith('/cambio'):
+            raise ValueError('Página Não Encontrada')
 
     def get_url_parametros(self):
         indice_interrogacao = self.url.find('?')
@@ -42,6 +41,13 @@ class ExtratorUrl:
         return valor
 
 
-extrator_url = ExtratorUrl("https://bytebank.com/cambio?quantidade=100&moedaDestino=Dolar&moedaOrigem=real")
+url_inicio = "https://bytebank.com/cambio?quantidade=100&moedaDestino=Dolar&moedaOrigem=real"
+extrator_url = ExtratorUrl(url_inicio)
+
 valor_quantidade = extrator_url.get_valor_parametro('quantidade')
-print('\n', valor_quantidade, '\n')
+valor_moeda_origem = extrator_url.get_valor_parametro('moedaOrigem')
+valor_moeda_destino = extrator_url.get_valor_parametro('moedaDestino')
+
+print('\n', valor_quantidade)
+print(valor_moeda_origem)
+print(valor_moeda_destino, '\n')
